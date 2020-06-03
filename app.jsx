4522ed8af8a8fd12d794c10686ca19bad6e98c50
MarianC312 <visual_design@live.com.ar>
2020-06-03 00:28:51 -0300

    fix error al eliminar items de lista.

--- app.jsx
+++ app.jsx
@@ -14,45 +14,43 @@ function Title() {
 }
 
 class Lista extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       input: "",
       task: [
         "React",
-        "PHP",
+        "PHP5",
         "CSS"
         ]
     }
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-  
-  handleChange(event){
+
+  handleChange(event) {
     this.setState({
       input: event.target.value
     });
   }
-  
-  handleClick(){
+
+  handleClick() {
     this.setState({
-      task: [...this.state.task,this.state.input],
+      task: [...this.state.task, this.state.input],
       input: ""
     })
   }
-  
-  deleteTask(event){
+
+  deleteTask = index => {
+    const list = this.state.task.filter((item,i) => i !== index);
     this.setState({
-      task:  this.state.task.filter((data) => {
-        data == event.target.value;
-      }).map(filtered => (filtrted))
-    })
+      task: [...list]
+    });
   }
-  
-  render(){
-    console.log(this.state.task);
+
+  render() {
     return (
-    <div>
+      <div>
       <div className="d-flex input-group justify-content-center p-2 mb-3">
         <input className="form-control" onChange={this.handleChange} value={this.state.input} type="text"></input>
         <div className="input-group-append">
@@ -63,12 +61,12 @@ class Lista extends React.Component {
         <ul className="list-group">
           {
             this.state.task.map((data, index) =>
-              <li className="list-group-item d-flex align-items-center" id={index} key={index}>
+              <li className="list-group-item d-flex align-items-center" key={index}>
                 <div className="mr-auto d-inline">
                   <i className="fa fa-caret-right text-primary mr-2"></i>
                   {data}
                 </div>
-                  <button value={data} index={index} onClick={this.deleteTask} className="btn btn-sm btn-danger">
+                  <button onClick={() => this.deleteTask(index)} className="btn btn-sm btn-danger">
                     <i className="fa fa-times"></i>
                   </button>
               </li>
@@ -82,12 +80,12 @@ class Lista extends React.Component {
 }
 
 function Main() {
-    return (
-      <div className="container text-center">
+  return (
+    <div className="container text-center">
         <Title/>
         <Lista />
       </div>
-    )
+  )
 }
 
 
